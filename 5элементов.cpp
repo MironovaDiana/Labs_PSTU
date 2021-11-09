42dfd8385c480744349aac8cfb773cb3849f7b47
Diana Mironova <92561994+MironovaDiana@users.noreply.github.com>
2021-11-09 19:54:32 +0500

    Add files via upload

---
+++ 5элементов.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	char a, b, c, d, e;
+	cout << "Введите 5 символов"
+	cin >> a >> b >> c >> d >> e;
+	cout << e << d << c << b << a;
+	return 0;
+}
+
