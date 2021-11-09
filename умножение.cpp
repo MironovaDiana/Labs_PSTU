18db767a02763a73465a06f1df8bddef99aeefbf
Diana Mironova <92561994+MironovaDiana@users.noreply.github.com>
2021-11-09 19:52:03 +0500

    Add files via upload

---
+++ умножение.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double a, b, c;
+	cout << "Введите числа через Enter" << endl;
+	cin >> a;
+	cin >> b;
+	c = a * b;
+	cout << "Результат умножения:" << c;
+	return 0;
+}
+
